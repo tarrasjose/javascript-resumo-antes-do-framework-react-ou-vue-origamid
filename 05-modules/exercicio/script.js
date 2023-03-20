ba489552d305ce3cf2732789ae474967f1c7c8ec
Tárras José <tarras_souza@hotmail.com>
2023-03-20 13:10:52 -0300

    Create script.js

---
+++ 05-modules/exercicio/script.js
@@ -0,0 +1,13 @@
+import numeroAleatorio from "./numeroAleatorio.js";
+import { areaQuadrada, perimetroQuadrado } from "./quadrado.js";
+import circulo from "./circulo.js";
+
+
+console.log(numeroAleatorio());
+
+console.log(areaQuadrada(4));
+console.log(perimetroQuadrado(3));
+
+console.log(circulo.area(4));
+console.log(circulo.circunferencia(3));
+console.log(circulo.aleatorio());
